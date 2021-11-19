2e49904cc5d7a18e79a127afae9ce371d7add67f
Brandon <bmcomis2018@gmail.com>
2021-11-19 06:58:26 -0600

    Start base for HiSockGroup

---
+++ hisock/groups.py
@@ -0,0 +1,28 @@
+from .client import HiSockClient
+from typing import Optional
+
+
+class HiSockGroup:
+    def __init__(
+            self,
+            server_conn: HiSockClient,
+            name: str,
+            clients: Optional[list[tuple[str, int], ...]] = None,
+            cache_size: int = -1,
+    ):
+        # Define later-to-be-used attrs
+        self.server_conn = server_conn
+        self.name = name
+        self.cache_size = cache_size
+
+        if cache_size >= 0:
+            # cache_size <= -1: No cache
+            self.cache = []
+
+        if clients is None:
+            self.clients = []
+        else:
+            # List elements should be (ip, port) style
+            self.clients = clients
+
+        # Contacts server
